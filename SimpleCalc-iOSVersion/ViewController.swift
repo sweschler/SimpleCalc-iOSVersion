12278aa522279f0614283422dfffd1e265c8b291
weschler <sabrinaweschler@me.com>
2015-10-20 22:28:42 -0700

    Initial Commit

---
+++ SimpleCalc-iOSVersion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalc-iOSVersion
+//
+//  Created by Sabrina Weschler on 10/20/15.
+//  Copyright © 2015 Sabrina Weschler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
